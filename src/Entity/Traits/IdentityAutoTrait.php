392cf77869fa1f9f7178ac8b0218c9a6f7d7fb31
Hugon Sknadaj <hugon94@gmail.com>
2017-11-07 19:42:21 +0100

    TASK-2 Stworzenie panelu logowania

---
+++ src/Entity/Traits/IdentityAutoTrait.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+namespace App\Entity\Traits;
+
+trait IdentityAutoTrait
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
