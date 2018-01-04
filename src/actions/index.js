ab15c1f7b3837d739a313b19fe91df6294ec16d0
Hugon Sknadaj <hugon94@gmail.com>
2018-01-04 17:57:07 +0100

    Usuniecie przypadkowego pulla z frontendu

--- src/actions/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const client = axios.create({
-    baseURL: 'http://imperius.home:8090/api',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
-});
